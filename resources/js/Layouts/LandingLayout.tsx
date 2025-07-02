2046972cbe242bed9f64058b0987aba5d21e2fc0
unknown <danapradana30@gmail.com>
2025-07-02 18:10:00 +0100

    Update Landing Page

--- resources/js/Layouts/LandingLayout.tsx
+++ resources/js/Layouts/LandingLayout.tsx
@@ -1,6 +1,7 @@
+import Navbar from '@/Components/Partials/Navbar';
 import { LayoutProps } from '@/types';
 import { Head } from '@inertiajs/react';
-import React from 'react'
+
 
 type Props = {}
 
@@ -8,8 +9,9 @@ const LandingLayout: React.FC<LayoutProps> = ({children, title}) => {
   return (
     <>
       <Head title={title}></Head>
+      <Navbar></Navbar>
       <div>
-        <main className='content'>
+        <main className='mt-16'>
           {children}
         </main>
       </div>
--- resources/js/Pages/Landing.tsx
+++ resources/js/Pages/Landing.tsx
@@ -6,31 +6,29 @@ type Props = {}
 const landing:React.FC =  ({}: Props) => {
   return (
     <LandingLayout title='Landing'>
-      <div className='grid grid-cols-1 md:grid-cols-2 justify-center md:pt-20 pb-96'>
+      <div className='grid grid-cols-1 md:grid-cols-2 justify-center md:pt-20 pb-96 bg-gradient-to-b from-red-500 to-red-600'>
+        <div className="w-full flex justify-center">
+            <div className="md:ml-40 md:me-10 mt-32">
+                <h1 className="text-4xl text-center text-gray-100 font-bold">Yuk, Belajar Bahasa Inggris Asik di Kampung Inggris LC!</h1>
 
-            <div className="w-full flex justify-center">
-                <div className="ml-40 me-10 mt-32">
-                    <p className="text-primary-font text-2xl mb-6 font-semibold">Hello Everyone, I Am</p>
-                    <h1 className="text-6xl text-[#120D40] font-bold">Cloudias Imani Pradana</h1>
-
-                    <div className="bg-primary-font p-5 rounded-md mt-16">
-                        <p className="text-white text-center text-2xl">Fullstack Developer Enthusiast</p>
-                    </div>
-                    <div className="mt-6">
-                        <button className="focus:outline-none text-white bg-[#120D40] hover:bg-[#221970] focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-xl px-5 py-2.5 me-2 mb-2">
-                            Contact Me
-                        </button>
-                    </div>
+                <div className="bg-primary-font p-5 rounded-md mt-4">
+                    <p className="text-white text-center text-xl">Metode fun dan English area, bikin kamu semangat belajar. Materi lengkap mulai dari grammar, pronunciation, dan speaking.</p>
+                </div>
+                <div className="flex justify-center pt-6">
+                    <button className="bg-yellow-400 hover:bg-yellow-500 text-black text-lg font-bold py-3.5 px-10 rounded-lg shadow-lg transition duration-300 hover:cursor-pointer">
+                        Join Sekarang
+                    </button>
                 </div>
             </div>
+        </div>
 
-             <div className="w-full">
-                <div className="relative w-full h-full mt-16 md:mt-0">
-                    <div className="absolute top-0 -z-10">
-                        <img src="/images/Pare.webp" alt="Picture Of Dana" className="h-[30em] md:h-full" />
-                    </div>
-                </div>
-             </div>
+        <div className="w-full">
+          <div className="w-full h-full mt-16 md:mt-0">
+              <div className="">
+                  <img src="/images/Pare.webp" alt="Picture Of Dana" className="h:full md:h-full" />
+              </div>
+          </div>
+        </div>
       </div>
     </LandingLayout>
   )
